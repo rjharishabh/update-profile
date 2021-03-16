674855817fedfc9cf4337b21337e5689c79bdc5d
rjharishabh <rjharishabh@gmail.com>
2021-03-16 11:04:00 +0530

    php code added for database updation and files upload

--- pr.php
+++ pr.php
@@ -1,3 +1,43 @@
+ <?php
+require_once "db.php";
+if (isset($_POST['name'])&&isset($_POST['about'])&&isset($_POST['loc'])) {
+
+
+  $target_dir = "imgs/";
+  $target_file = $target_dir . basename($_FILES["picFile"]["name"]);
+  // $uploadOk = 1;
+  // $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  move_uploaded_file($_FILES["picFile"]["tmp_name"], $target_file);
+
+
+  $dom=new DOMDocument();
+if($dom->getElementById('tfa')){
+$query="INSERT INTO DET(name,about,location,tfa) VALUES (:name,:about,:loc,:tfa)";
+$stmt=$db->prepare($query);
+$stmt->execute(array(':name' => $_POST['name'],
+':about'=>$_POST['about'],
+':loc'=>$_POST['loc'],
+":tfa"=>2
+ ));
+}
+else {
+$query="INSERT INTO DET(name,about,location,tfa) VALUES (:name,:about,:loc,:tfa)";
+$stmt=$db->prepare($query);
+$stmt->execute(array(':name' => $_POST['name'],
+':about'=>$_POST['about'],
+':loc'=>$_POST['loc'],
+":tfa"=>1
+ ));
+}
+
+
+}
+
+
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -9,69 +49,72 @@
   <body>
 <div class="container">
   <?php echo "<p style='float:right';>Your ip address is ".$_SERVER['REMOTE_ADDR']."</p>" ?>
+  <!-- <button style='float:right;'  class="btn btn-sm btn-primary">Edit</button> -->
   <div class="profile">
      <img src="imgs/blank.png" alt="profile-pic">
-  <h2>Full Name</h2>
+
+<form action="pr.php" enctype="multipart/form-data" method="post">
+     <input type="file" name="picFile" value="">
+  <input type="text" name="name" placeholder="Full Name">
   </div>
-<div class="details">
+  <div class="details">
   <div class="row">
-    <h5>About me</h5>
+  <h5>About me</h5>
   </div>
   <div class="row">
-    <div id="about-me">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis venenatis ex.
-    Suspendisse massa velit, finibus ut consequat id, malesuada sit amet leo. Praesent sodales non sapien et laoreet.
-    Morbi lobortis quam erat, quis cursus nibh fermentum at. Aenean nisi metus, cursus non rhoncus a, laoreet in dolor.
-    Donec fringilla elit et odio imperdiet,
-    </div>
+   <textarea name="about" rows="4" cols="800"></textarea>
   </div>
 
 
-<div class="row">
-<div class="col-md-6">
+  <div class="row">
+  <div class="col-md-6">
   <h5>Username</h5>
-</div>
-<div class="col-md-6">
+  </div>
+  <div class="col-md-6">
   <h5>Password</h5>
-</div>
-</div>
-<div class="row">
+  </div>
+  </div>
+  <div class="row">
   <div class="col-md-6">
-    <input type="text" name="" value="">
+  <input type="text" name="username" value="">
   </div>
   <div class="col-md-6">
-    <input type="text" name="" value="">
+  <input type="text" name="password" value="">
   </div>
-</div>
+  </div>
 
-<div class="row">
-<div class="col-md-6">
+  <div class="row">
+  <div class="col-md-6">
   <h5>Email</h5>
-</div>
-<div class="col-md-6">
+  </div>
+  <div class="col-md-6">
   <h5>Location</h5>
-</div>
-</div>
+  </div>
+  </div>
 
-<div class="row">
+  <div class="row">
   <div class="col-md-6">
-    <input type="text" name="" value="">
+  <input type="text" name="email" value="">
   </div>
   <div class="col-md-6">
-    <input type="text" name="" value="">
+  <input type="text" name="loc" value="">
   </div>
-</div>
+  </div>
 
-<div class="row">
-<label for="tfa">Two Factor Authentication</label>
-<div class="custom-control custom-switch">
+  <div class="row">
+  <label for="tfa">Two Factor Authentication</label>
+  <div class="custom-control custom-switch">
   <input type="checkbox" class="custom-control-input" id="tfa">
-    <label class="custom-control-label" for="tfa"></label>
-</div>
-</div>
+  <label class="custom-control-label" for="tfa"></label>
+  </div>
+  </div>
 
 
-</div>
+  </div>
+<input type="submit" value="Submit">
+
+</form>
+
 
 
 
